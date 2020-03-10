f2cd46bc23f4dcfeedc0a5dd54921ebeb4a9c1f7
Subham Sharma <ksubham.sharma@gmail.com>
2020-03-10 18:17:03 +0530

    changesss

--- app.js
+++ app.js
@@ -12,16 +12,7 @@ GAME RULES:
 
 var scores, roundScore, activePlayer
 
-score = [0,0]
-roundScore  = 0
-activePlayer = 0
-
-document.querySelector('.dice').style.display = 'none'
-
-document.getElementById('score-0').textContent ="0"
-document.getElementById('score-1').textContent ="0"
-document.getElementById('current-0').textContent ="0"
-document.getElementById('current-1').textContent ="0"
+init()
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
     var dice = Math.floor(Math.random()*6)+1
@@ -32,30 +23,76 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
         roundScore +=dice;
         document.querySelector('#current-'+activePlayer).textContent = roundScore
     } else {
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
-        roundScore = 0
-
-        document.getElementById('current-0').textContent = '0'
-        document.getElementById('current-1').textContent = '0'
-
-        document.querySelector('.player-0-panel').classList.toggle('active')
-        document.querySelector('.player-1-panel').classList.toggle('active')
-
-        document.querySelector('.dice').style.display = 'none'
-
-        
-        //document.querySelector('.player-0-panel').classList.remove('active')
-        //document.querySelector('.player-1-panel').classList.add('active')
-
-
-
+        scores[activePlayer]+=roundScore
+        document.querySelector('#score-'+activePlayer).textContent = scores[activePlayer]
+        if(scores[activePlayer] >= 20){
+            winner()
+        } else {
+            nextPlayer()
+        }
     }
 });
 
-document.querySelector(',btn-hold').addEventListener('click', function(){
-
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    scores[activePlayer]+=roundScore
+    document.querySelector('#score-'+activePlayer).textContent = scores[activePlayer]
+    
+    if(scores[activePlayer] >= 20){
+        winner()
+    } else {
+        nextPlayer()
+    }
 });
 
+document.querySelector('.btn-new').addEventListener('click',function(){
+    init()
+})
+
+
+function init(){
+    scores = [0,0]
+    roundScore  = 0
+    activePlayer = 0
+
+    document.querySelector('.dice').style.display = 'none'
+
+    document.getElementById('score-0').textContent ="0"
+    document.getElementById('score-1').textContent ="0"
+    document.getElementById('current-0').textContent ="0"
+    document.getElementById('current-1').textContent ="0"
+    document.getElementById('name-0').textContent = "Player 1"
+    document.getElementById('name-1').textContent = "Player 2"
+    
+    document.querySelector('.player-0-panel').classList.remove('winner')
+    document.querySelector('.player-1-panel').classList.remove('winner')
+    document.querySelector('.player-0-panel').classList.remove('active')
+    document.querySelector('.player-1-panel').classList.remove('active')
+
+    document.querySelector('.player-0-panel').classList.add('active')
+}
+
+function nextPlayer(){
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
+    roundScore = 0
+
+    document.getElementById('current-0').textContent = '0'
+    document.getElementById('current-1').textContent = '0'
+
+    document.querySelector('.player-0-panel').classList.toggle('active')
+    document.querySelector('.player-1-panel').classList.toggle('active')
+
+    document.querySelector('.dice').style.display = 'none'
+
+    //document.querySelector('.player-0-panel').classList.remove('active')
+    //document.querySelector('.player-1-panel').classList.add('active')
+}
+
+function winner(){
+    document.querySelector('#name-'+activePlayer).textContent = 'Winner!'
+    document.querySelector('.dice').style.display = 'none'
+    document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner')
+    document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active')
+}
 
 
 // document.querySelector('#current-'+activePlayer).textContent = dice
